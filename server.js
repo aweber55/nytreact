14058399d8fb5fea25d2d37c3d14de5fe0842b38
Ashley Weber <aweber333@yahoo.com>
2018-09-25 11:35:18 -0400

    mongoose

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
 // Send every request to the React app
 // Define any API routes before this runs
